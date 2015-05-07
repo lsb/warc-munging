ee1177304943bcea6520e0c1693fb22c0366432f
lsb <leebutterman@gmail.com>
2015-05-06 18:26:15 -0700

    Update get-all-warc-paths.sh
    Missing path separator

--- get-all-warc-paths.sh
+++ get-all-warc-paths.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget --quiet --output-document=- https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/warc.paths.gz | gzip -cd | sed 's_^_https://aws-publicdatasets.s3.amazonaws.com_' | sort
+wget --quiet --output-document=- https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/warc.paths.gz | gzip -cd | sed 's_^_https://aws-publicdatasets.s3.amazonaws.com/_' | sort
